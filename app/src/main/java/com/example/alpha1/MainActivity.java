eef3e4d54eaaedc4f05f27fc240779eaf4bdf4a4
adidi3 <ap0413@bs.amalnet.k12.il>
2019-10-10 08:23:33 +0300

    test commit

--- app/src/main/java/com/example/alpha1/MainActivity.java
+++ app/src/main/java/com/example/alpha1/MainActivity.java
@@ -3,16 +3,32 @@ package com.example.alpha1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * @author Adi
  */
 
 public class MainActivity extends AppCompatActivity {
+    EditText tphone, tmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tphone=(EditText)findViewById(R.id.phone);
+        tmail=(EditText)findViewById(R.id.mail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void EnterData(View view) {
+
     }
 }
